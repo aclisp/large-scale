4e067affa7fd5f1a9dbac1e561da4d7816767c9e
aclisp <aclisp@gmail.com>
2013-09-22 12:41:58 +0800

    call background processes in python 2.2

---
+++ aprlab/exampleBackground22.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+import popen2
+import os
+
+def explainStatus(statusCode):
+    signal = statusCode & 0xFF
+    exitCode = statusCode >> 8
+    if signal == 0:
+        return 'exit %d' % exitCode
+    else:
+        return 'signal %d' % signal
+
+repos = ['/etc', '/proc', '/var']
+# launch 3 async calls:
+procs = [popen2.Popen3(r'ls -l %s > %s.log 2>&1'%(repo,repo.lstrip('/'))) for repo in repos]
+# wait.
+stats = [proc.wait() for proc in procs]
+# check for results:
+for k, v in zip(repos, [explainStatus(stat) for stat in stats]):
+    print k, v
+
